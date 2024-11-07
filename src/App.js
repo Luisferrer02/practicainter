dbda63cd2d952c6011d407dcb8ea25755a805e2c
Luisferrer02 <luisferrer2002@gmail.com>
2024-11-07 22:59:14 +0100

    env vars

--- src/App.js
+++ src/App.js
@@ -8,17 +8,27 @@ function App() {
   const [comics, setComics] = useState([]);
   const [filterText, setFilterText] = useState('');
 
-  const publicKey = '3020f4197a307f61356112d94191bbd8';
-  const privateKey = 'e8f91f88c8784d764a685e9370d1d7c55ce7fadb';
+  const publicKey = process.env.REACT_APP_PUBLIC_KEY;
+  const privateKey = process.env.REACT_APP_PRIVATE_KEY;
   const ts = Date.now().toString();
   const hash = md5(ts + privateKey + publicKey).toString();
 
-  const url = `https://gateway.marvel.com/v1/public/comics?orderBy=-modified&ts=${ts}&apikey=${publicKey}&hash=${hash}`;
-  console.log(url);
-  fetch(url)
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error('Error fetching data:', error));
+  useEffect(() => {
+    const fetchComics = async () => {
+      //const url = `https://gateway.marvel.com/v1/public/comics?orderBy=-modified&apikey=${publicKey}`;
+      const url = `https://gateway.marvel.com/v1/public/comics?orderBy=-modified&ts=${ts}&apikey=${publicKey}&hash=${hash}`;
+      console.log(url);
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        setComics(data.data.results);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchComics();
+  }, [hash, publicKey, ts]);
 
   return (
     <div className="App">
